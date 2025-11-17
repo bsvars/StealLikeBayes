ba36bcfdaf2f296d47b8797af2f656d274af1256
Tomasz Woźniak <wozniak.tom@pm.me>
2025-11-17 22:43:24 +1100

    keep it simple

--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,3 @@
 if ( requireNamespace("tinytest", quietly = TRUE) ) {
-  # To skip running the tests on CRAN make them run only on the developer version 
-  home <- length(unclass(packageVersion("StealLikeBayes"))[[1]]) == 4
-  tinytest::test_package("StealLikeBayes", at_home = home)
+  tinytest::test_package("StealLikeBayes")
 }
