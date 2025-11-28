b5572a8c2da3b2f50a3baef6aebec0aa0e7cad99
Tomasz Woźniak <wozniak.tom@pm.me>
2025-11-28 18:11:58 +1100

    update of the cpp code #27

--- src/rgig1.cpp
+++ src/rgig1.cpp
@@ -1,10 +1,23 @@
-#include <Rcpp.h>
+
+#include <RcppArmadillo.h>
 #include <math.h>
 #include <float.h>
 
 using namespace Rcpp;
+using namespace arma;
 
-double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega, double alpha){
+/* This file was stolen by Tomasz from package shrinkTVP by Peter Knaus
+ * on 28 November 2025 and then rewritten.
+ */
+
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
+double univar_rgig_newapproach1 (
+    double lambda, 
+    double lambda_old, 
+    double omega, 
+    double alpha
+) {
 
   double A[3], Atot;
   double k0;
@@ -22,19 +35,11 @@ double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega,
 
   if (lambda >= 1. || omega >1.)
     throw ("invalid parameters");
-
-
-
-
+  
   xm = omega / (std::sqrt((1.-lambda)*(1.-lambda) + omega*omega)+(1.-lambda));
-
-
   x0 = omega/(1.-lambda);
-
-
   k0 = std::exp((lambda-1.)*std::log(xm) - 0.5*omega*(xm + 1./xm));
   A[0] = k0 * x0;
-
 
   if (x0 >= 2./omega) {
     k1 = 0.;
@@ -50,29 +55,20 @@ double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega,
       ? k1 * std::log(2./(omega*omega))
         : k1 / lambda * ( std::pow(2./omega, lambda) - std::pow(x0, lambda) );
 
-
     k2 = std::pow(2/omega, lambda-1.);
     A[2] = k2 * 2 * std::exp(-1.)/omega;
   }
 
-
   Atot = A[0] + A[1] + A[2];
-
   do {
     ++count;
-
-
-    V = Atot * R::runif(0, 1);
-
+    V = Atot * randu();
     do {
-
-
       if (V <= A[0]) {
         X = x0 * V / A[0];
         hx = k0;
         break;
       }
-
 
       V -= A[0];
       if (V <= A[1]) {
@@ -87,7 +83,6 @@ double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega,
         break;
       }
 
-
       V -= A[1];
       a = (x0 > 2./omega) ? x0 : 2./omega;
       X = -2./omega * std::log(std::exp(-omega/2. * a) - omega/(2.*k2) * V);
@@ -96,11 +91,8 @@ double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega,
 
     } while(0);
 
-
-    U = R::runif(0, 1) * hx;
-
+    U = randu() * hx;
     if (std::log(U) <= (lambda-1.) * std::log(X) - omega/2. * (X+1./X)) {
-
       res = (lambda_old < 0.) ? (alpha / X) : (alpha * X);
       break;
     }
@@ -110,9 +102,13 @@ double univar_rgig_newapproach1 (double lambda, double lambda_old, double omega,
 }
 
 
-double do_rgig1(double lambda,
-                double chi,
-                double psi) {
+// [[Rcpp::interfaces(cpp)]]
+// [[Rcpp::export]]
+double rgig1(
+    double lambda,
+    double chi,
+    double psi
+) {
 
   if (chi == 0){
     chi = DBL_MIN;
@@ -132,20 +128,20 @@ double do_rgig1(double lambda,
   if ((chi < (11 * DBL_EPSILON)) & (lambda != 0)) {
     /* special cases which are basically Gamma and Inverse Gamma distribution */
     if (lambda > 0.0) {
-      res = R::rgamma(lambda, 2.0/psi);
+      res = randg(distr_param(lambda, 2.0/psi));
     }
     else {
-      res = 1.0/R::rgamma(-lambda, 2.0/chi); // fixed
+      res = 1.0/randg(distr_param(-lambda, 2.0/chi)); // fixed
     }
   }
 
   else if ((psi < (11 * DBL_EPSILON)) & (lambda != 0)) {
     /* special cases which are basically Gamma and Inverse Gamma distribution */
     if (lambda > 0.0) {
-      res = R::rgamma(lambda, 2.0/psi);  // fixed
+      res = randg(distr_param(lambda, 2.0/psi));  // fixed
     }
     else {
-      res = 1.0/R::rgamma(-lambda, 2.0/chi); // fixed
+      res = 1.0/randg(distr_param(-lambda, 2.0/chi)); // fixed
     }
 
   } else if ((lambda == 0) && (sqrt(psi*chi) > 0) && (sqrt(psi*chi) < 1)) {
